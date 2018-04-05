b4c3c29c833309aec9f6a36f8b094c78d1d0cebe
michael <michael@iseardmedia.com>
2018-04-05 09:48:59 +0200

    update build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp = require('gulp')
-let sass = require('gulp-sass')
-let uglify = require('gulp-uglify-es').default
-let gutil = require('gulp-util')
-let sourcemaps = require('gulp-sourcemaps')
-let autoprefixer = require('gulp-autoprefixer')
-let browserSync = require('browser-sync').create()
-let purify = require('gulp-purifycss')
-let cleanCSS = require('gulp-clean-css')
-let rename = require("gulp-rename")
+var pump = require('pump')
+var sass = require('gulp-sass')
+let uglify = require('gulp-uglify-es').default;
+var sourcemaps = require('gulp-sourcemaps')
+var autoprefixer = require('gulp-autoprefixer')
+var browserSync = require('browser-sync').create()
+var cleanCSS = require('gulp-clean-css')
+var rename = require("gulp-rename")
 
 // folders
 let folder = {
@@ -24,17 +23,26 @@ gulp.task('sass', function () {
   return gulp.src(folder.src + 'sass/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({browsers: ['last 2 versions', '> 5%', 'Firefox ESR']}))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }))
     .pipe(sourcemaps.write())
     .pipe(browserSync.stream())
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({
+      compatibility: 'ie8'
+    }))
     .pipe(gulp.dest(folder.dist + 'css'))
 })
 
-gulp.task('js', function () {
-  return gulp.src(folder.src + 'js/*.js')
-    .pipe(uglify())
-    .pipe(rename('skroll-slider.min.js'))
-    .pipe(gulp.dest(folder.dist + 'js/')) // Start piping stream to tasks!
-  browserSync.reload()
+gulp.task('js', function (cb) {
+  pump([
+      gulp.src(folder.src + 'js/*.js'),
+      sourcemaps.init(),
+      uglify(),
+      sourcemaps.write(),
+      rename('skroll-slider.min.js'),
+      gulp.dest(folder.dist + 'js/')
+    ],
+    cb
+  );
 })
