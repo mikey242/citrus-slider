f81afc0f4743aabcbb43ae840960a1629e4151ed
michael <michael@iseardmedia.com>
2018-04-05 12:09:09 +0200

    update slider object structure

--- src/js/skroll-slider.js
+++ src/js/skroll-slider.js
@@ -3,19 +3,6 @@ let sliders = document.getElementsByClassName('skroll-slider')
 var sliderObjects = []
 
 // CREATE SLIDER OBJECTS
-// for (var i = 0; i < sliders.length; i++) {
-//   var slidesWrap = sliders[i].querySelector('.slides')
-//   var slides = sliders[i].getElementsByClassName('slide-wrap')
-//   var arrows = sliders[i].getElementsByClassName('slide-arrow')
-//   var num = slides.length
-//   var duration = (sliders[i].dataset.duration.length ? sliders[i].dataset.duration : 5000)
-//   var slider = {slider: sliders[i], slidesWrap: slidesWrap, paused: false, slideIndex: 0, slides: slides, arrows: arrows, num: num, height: 'auto', duration: duration}
-//   sliderObjects[i] = slider
-//   sliderInit(sliderObjects[i])
-//   autoSlide(sliderObjects[i])
-// }
-
-// CREATE SLIDER OBJECTS
 for (var i = 0; i < sliders.length; i++) {
   var el = sliders[i]
   var num = el.children.length
@@ -24,17 +11,27 @@ for (var i = 0; i < sliders.length; i++) {
   var imgUrls = Array.from(el.querySelectorAll('img')).map(function(e,i) {
     return e.src
   })
-  var duration = (el.hasAttribute('data-duration') ? el.dataset.duration : 5000)
-  var zoom = (el.classList.contains('zoom') ? true : false)
-  sliderObjects[i] = {slider: el, imgElements: imgElements, num: num, zoom: zoom, paused: false, slideIndex: 0, slides: slides, imgUrls: imgUrls, duration: duration}
+  // set defaults
+  sliderObjects[i] = {slider: el, img: imgElements, slides: slides, imgUrls: imgUrls, settings:{num: num, zoom: true, paused: false, slideIndex: 0, duration: 5000, transition: "pan"}}
+  // get settings from data attribute
+  var settings = JSON.parse(el.dataset.skroll)
+  // update object
+  for (const key of Object.keys(settings)) {
+    sliderObjects[i].settings[key] = settings[key]
+  }
   sliderConstruct(sliderObjects[i])
+  autoSlide(sliderObjects[i])
 }
 
+// CONSTRUCT DOM ELEMENTS
 function sliderConstruct(e) {
+
+  // set container classes
+  e.slider.classList.add((e.settings.zoom ? 'zoom' : null), e.settings.transition)
 
   var fragment = document.createDocumentFragment();
 
-  // CREATE ARROWS
+  // create arrows
   var arrowContainer = document.createElement("DIV")
   arrowContainer.setAttribute("class", "arrows")
   var arrowLeft = document.createElement("DIV")
@@ -46,11 +43,15 @@ function sliderConstruct(e) {
   fragment.prepend(arrowContainer)
   e.arrows = [arrowLeft,arrowRight]
 
-  // CREATE SLIDES ELEMENTS
+  // create slide elements
   var slidesContainer = document.createElement("DIV")
   slidesContainer.setAttribute("class", "slides")
-  for (let i = 0; i < e.num; i++) {
-    e.slides[i].removeChild(e.imgElements[i])
+  // set container width
+  if (e.settings.transition==="pan") {
+    slidesContainer.style.width = e.settings.num + '00%'
+  }
+  for (let i = 0; i < e.settings.num; i++) {
+    e.slides[i].removeChild(e.img[i])
     if (i===0) {
       e.slides[i].classList.add('slide-wrap', 'current-slide')
     } else {
@@ -63,13 +64,13 @@ function sliderConstruct(e) {
     slidesContainer.appendChild(e.slides[i])
   }
   fragment.appendChild(slidesContainer)
-  delete e.imgElements
+  delete e.img
 
-  // CREATE INDICATORS ELEMENTS
+  // create indicator elements
   e.indicators = {}
   var indicatorsContainer = document.createElement("DIV")
   indicatorsContainer.setAttribute("class", "indicators")
-    for (let i = 0; i < e.num; i++) {
+    for (let i = 0; i < e.settings.num; i++) {
     var indicator = document.createElement("SPAN");
     if (i===0) {
       indicator.setAttribute("class","slide-indicator current-indicator")
@@ -82,36 +83,37 @@ function sliderConstruct(e) {
     e.indicators[i] = indicator
   }
 
-  // APPLY CHANGES TO DOM
+  // apply changes to dom
   e.slider.appendChild(fragment)
   console.log(e)
   setBindings(e)
 }
 
+// CREATE BINDINGS FOR ARROWS AND INDICATORS
 function setBindings(e) {
 
-    // BIND ARROW CLICK FUNCTIONS
+    // bind arrow click functions
     for (var i = 0; i < e.arrows.length; i++) {
       e.arrows[i].addEventListener('click', function (el) {
-        e.prevSlideIndex = e.slideIndex
+        e.prevSlideIndex = e.settings.slideIndex
         if (el.target.classList.contains('left-arrow')) {
-          var num = e.slideIndex-1
+          var num = e.settings.slideIndex-1
         } else if (el.target.classList.contains('right-arrow')) {
-          var num = e.slideIndex+1
+          var num = e.settings.slideIndex+1
         }
         updateIndex(e,num)
         clearTimeouts(e)
-        e.paused = true
+        e.settings.paused = true
         sliderChange(e)
       })
     }
   
-    // BIND INDICATOR CLICK FUNCTIONS
-    for (i = 0; i < e.num; i++) {
+    // bind indicator click functions
+    for (i = 0; i < e.settings.num; i++) {
       e.indicators[i].addEventListener('click', function (el) {
         clearTimeouts(e)
         updateIndex(e,Number(el.target.dataset.slide))
-        e.paused = true
+        e.settings.paused = true
         if (el.target.classList.contains('current-indicator') === false) {
           sliderChange(e)
         }
@@ -125,12 +127,12 @@ function setBindings(e) {
 //   e.height = 0
 //   // SET WIDTH
 //   if (e.slider.classList.contains('pan')) {
-//     // e.slidesWrap.style.width = e.num + '00%'
+//     // e.slidesWrap.style.width = e.settings.num + '00%'
 //   }
 //   // SET FIRST SLIDE AS CURRENT
 //   e.slides[0].classList.add('current-slide')
 //   e.indicators = {}
-//   for (let i = 0; i < e.num; i++) {
+//   for (let i = 0; i < e.settings.num; i++) {
 //     e.slides[i].setAttribute('data-slide', i)
 //     // CREATE INDICATORS
 //     var indicator = document.createElement("SPAN");
@@ -154,25 +156,25 @@ function setBindings(e) {
 //   // BIND ARROW CLICK FUNCTIONS
 //   for (var i = 0; i < e.arrows.length; i++) {
 //     e.arrows[i].addEventListener('click', function (el) {
-//       e.prevSlideIndex = e.slideIndex
+//       e.prevSlideIndex = e.settings.slideIndex
 //       if (el.target.classList.contains('arrow-left')) {
-//         var num = e.slideIndex-1
+//         var num = e.settings.slideIndex-1
 //       } else if (el.target.classList.contains('arrow-right')) {
-//         var num = e.slideIndex+1
+//         var num = e.settings.slideIndex+1
 //       }
 //       updateIndex(e,num)
 //       clearTimeouts(e)
-//       e.paused = true
+//       e.settings.paused = true
 //       sliderChange(e)
 //     })
 //   }
 
 //   // BIND INDICATOR CLICK FUNCTIONS
-//   for (i = 0; i < e.num; i++) {
+//   for (i = 0; i < e.settings.num; i++) {
 //     e.indicators[i].addEventListener('click', function (el) {
 //       clearTimeouts(e)
 //       updateIndex(e,Number(el.target.dataset.slide))
-//       e.paused = true
+//       e.settings.paused = true
 //       if (el.target.classList.contains('current-indicator') === false) {
 //         sliderChange(e)
 //       }
@@ -183,31 +185,31 @@ function setBindings(e) {
 function sliderChange (e) {
   e.prev = e.slider.getElementsByClassName('current-slide')[0]
 
-  // CALCULATE SLIDE INDEX
-  if (e.slideIndex < 0) {
-    e.slideIndex = e.num - 1
-    if (e.num) { e.indicators[0].parentElement.classList.add('transition-last') }
+  // calculate slide index
+  if (e.settings.slideIndex < 0) {
+    e.settings.slideIndex = e.settings.num - 1
+    if (e.settings.num) { e.indicators[0].parentElement.classList.add('transition-last') }
     setTimeout(function () {
-      if (e.num) { e.indicators[0].parentElement.classList.remove('transition-last') }
+      if (e.settings.num) { e.indicators[0].parentElement.classList.remove('transition-last') }
     }, 1000)
-  } else if (e.slideIndex > e.num - 1) {
-    e.slideIndex = 0
-    if (e.num) { e.indicators[0].parentElement.classList.add('transition-first') }
+  } else if (e.settings.slideIndex > e.settings.num - 1) {
+    e.settings.slideIndex = 0
+    if (e.settings.num) { e.indicators[0].parentElement.classList.add('transition-first') }
     setTimeout(function () {
-      if (e.num) { e.indicators[0].parentElement.classList.remove('transition-first') }
+      if (e.settings.num) { e.indicators[0].parentElement.classList.remove('transition-first') }
     }, 1000)
   }
 
-  // SET CLASSES BASED ON SLIDE INDEX
-  if (e.num > 1) {
+  // set classes based on slide index
+  if (e.settings.num > 1) {
     // remove current styles
     e.slider.classList.remove('forwards', 'backwards')
-    for (var i = 0; i < e.num; i++) {
+    for (var i = 0; i < e.settings.num; i++) {
       e.slides[i].classList.remove('current-slide', 'prev-slide', 'next-slide')
-      if (e.num) { e.indicators[i].classList.remove('current-indicator') }
+      if (e.settings.num) { e.indicators[i].classList.remove('current-indicator') }
     }
     // add direction class
-    if (e.prevSlideIndex < e.slideIndex) {
+    if (e.prevSlideIndex < e.settings.slideIndex) {
       e.slider.classList.add('forwards')
     } else {
       e.slider.classList.add('backwards')
@@ -218,47 +220,47 @@ function sliderChange (e) {
     // add previous slide
     e.prev.classList.add('prev-slide')
 
-    if (e.slideIndex === e.num - 1) {
+    if (e.settings.slideIndex === e.settings.num - 1) {
       e.slides[0].classList.add('next-slide')
     } else {
-      if (e.slides[e.slideIndex + 1]) {
-        e.slides[e.slideIndex + 1].classList.add('next-slide')
+      if (e.slides[e.settings.slideIndex + 1]) {
+        e.slides[e.settings.slideIndex + 1].classList.add('next-slide')
       }
     }
 
-    e.slides[e.slideIndex].classList.add('current-slide')
-    if (e.num) { e.indicators[e.slideIndex].classList.add('current-indicator') }
-
-    if (e.slider.classList.contains('pan')) {
-      e.slidesWrap.style.transform = 'translateX(-' + e.slideIndex / e.num * 100 + '00%)'
+    e.slides[e.settings.slideIndex].classList.add('current-slide')
+    if (e.settings.num) { e.indicators[e.settings.slideIndex].classList.add('current-indicator') }
+    console.log(e)
+    if (e.settings.transition==="pan") {
+      e.slider.children[1].style.transform = 'translateX(-' + e.settings.slideIndex / e.settings.num * 100 + '00%)'
     }
 
     e.intervalPrevAnim = setTimeout(function () {
       e.slider.classList.remove('animating')
     }, 1500)
-    if (e.paused === false) {
+    if (e.settings.paused === false) {
       autoSlide(e)
     }
   }
 }
 
 function updateIndex (e,n) {
-  e.prevSlideIndex = e.slideIndex
-  e.slideIndex = n
+  e.prevSlideIndex = e.settings.slideIndex
+  e.settings.slideIndex = n
 }
 
 function autoSlide (e) {
   e.intervalSlideChange = setTimeout(function () {
-    var num = e.slideIndex+1
+    var num = e.settings.slideIndex+1
     updateIndex(e,num)
     sliderChange(e)
-  }, e.duration)
+  }, e.settings.duration)
 }
 
 function clearTimeouts (e) {
   clearTimeout(e.intervalSlideChange)
   clearTimeout(e.intervalPrevAnim)
-  for (var i = 0; i < e.num; i++) {
+  for (var i = 0; i < e.settings.num; i++) {
     e.slides[i].classList.remove('prev-anim')
   }
 }
