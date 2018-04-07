da20aa36199b4dd5370ab94301d9496439aad037
michael <michael@iseardmedia.com>
2018-04-07 17:57:13 +0200

    allow start slide and hide indicators to be configured

--- src/js/skroll-slider.js
+++ src/js/skroll-slider.js
@@ -21,10 +21,11 @@ for (var i = 0; i < sliders.length; i++) {
     img: imgElements,
     slideText: slideText,
     imgUrls: imgUrls,
+    num: num,
     settings: {
-      num: num,
       effect: 'zoom',
       animateText: true,
+      showIndicators: true,
       paused: false,
       slideIndex: 0,
       duration: 5000,
@@ -67,28 +68,28 @@ function sliderConstruct(e) {
   e.slides = slides
   slides.classList.add("slides")
   if (e.settings.transition==="pan") {
-    slides.style.width = e.settings.num + '00%'
+    slides.style.width = e.num + '00%'
   }
   // set container width
   if (e.settings.transition==="pan") {
-    slides.style.width = e.settings.num + '00%'
+    slides.style.width = e.num + '00%'
   }
 
   var slideFragment = {}
   slideFragment.slide = null
   slideFragment.slideWrap = null
 
-  for (let i = 0; i < e.settings.num; i++) {
+  for (let i = 0; i < e.num; i++) {
     slideFragment.slideWrap = document.createElement("DIV")
-    if (i===0) {
-      slideFragment.slideWrap.classList.add('slide-wrap', 'current-slide')
+    if (i===e.settings.slideIndex) {
+      slideFragment.slideWrap.setAttribute("class", "slide-wrap current-slide")
     } else {
-      slideFragment.slideWrap.classList.add('slide-wrap')
+      slideFragment.slideWrap.setAttribute("class", "slide-wrap")
     }
     slideFragment.slide = document.createElement("DIV")
-    slideFragment.slide.classList.add("slide")
+    slideFragment.slide.setAttribute("class","slide")
     slideFragment.slideText = e.slideText[i]
-    slideFragment.slideText.classList.add("slide-text")
+    slideFragment.slideText.setAttribute("class","slide-text")
     slideFragment.slide.style.backgroundImage = "url(" + e.imgUrls[i] + ")"
     slideFragment.slideWrap.appendChild(slideFragment.slide)
     slideFragment.slideWrap.appendChild(slideFragment.slideText)
@@ -99,10 +100,14 @@ function sliderConstruct(e) {
   // create indicator elements
   e.indicators = {}
   var indicatorsContainer = document.createElement("DIV")
-  indicatorsContainer.setAttribute("class", "indicators")
-  for (let i = 0; i < e.settings.num; i++) {
+  if (e.settings.showIndicators==false) {
+    indicatorsContainer.setAttribute("class", "indicators hidden")
+  } else {
+    indicatorsContainer.setAttribute("class", "indicators")
+  }
+  for (let i = 0; i < e.num; i++) {
     var indicator = document.createElement("SPAN");
-    if (i === 0) {
+    if (i === e.settings.slideIndex) {
       indicator.setAttribute("class", "slide-indicator current-indicator")
     } else {
       indicator.setAttribute("class", "slide-indicator")
@@ -142,7 +147,7 @@ function setBindings(e) {
   }
 
   // bind indicator click functions
-  for (i = 0; i < e.settings.num; i++) {
+  for (i = 0; i < e.num; i++) {
     e.indicators[i].addEventListener('click', function (el) {
       if (e.slider.classList.contains("animating")) {
         return;
@@ -162,34 +167,34 @@ function sliderChange(e) {
 
   // calculate slide index
   if (e.settings.slideIndex < 0) {
-    e.settings.slideIndex = e.settings.num - 1
-    if (e.settings.num) {
+    e.settings.slideIndex = e.num - 1
+    if (e.num) {
       e.indicators[0].parentElement.classList.add('transition-last')
     }
     setTimeout(function () {
-      if (e.settings.num) {
+      if (e.num) {
         e.indicators[0].parentElement.classList.remove('transition-last')
       }
     }, 1000)
-  } else if (e.settings.slideIndex > e.settings.num - 1) {
+  } else if (e.settings.slideIndex > e.num - 1) {
     e.settings.slideIndex = 0
-    if (e.settings.num) {
+    if (e.num) {
       e.indicators[0].parentElement.classList.add('transition-first')
     }
     setTimeout(function () {
-      if (e.settings.num) {
+      if (e.num) {
         e.indicators[0].parentElement.classList.remove('transition-first')
       }
     }, 1000)
   }
 
   // set classes based on slide index
-  if (e.settings.num > 1) {
+  if (e.num > 1) {
     // remove current styles
     e.slider.classList.remove('forwards', 'backwards')
-    for (var i = 0; i < e.settings.num; i++) {
+    for (var i = 0; i < e.num; i++) {
       e.slides.children[i].classList.remove('current-slide', 'prev-slide', 'next-slide')
-      if (e.settings.num) {
+      if (e.num) {
         e.indicators[i].classList.remove('current-indicator')
       }
     }
@@ -205,7 +210,7 @@ function sliderChange(e) {
     // add previous slide
     e.prev.classList.add('prev-slide')
 
-    if (e.settings.slideIndex === e.settings.num - 1) {
+    if (e.settings.slideIndex === e.num - 1) {
       e.slides.children[0].classList.add('next-slide')
     } else {
       if (e.slides.children[e.settings.slideIndex + 1]) {
@@ -214,19 +219,17 @@ function sliderChange(e) {
     }
 
     e.slides.children[e.settings.slideIndex].classList.add('current-slide')
-    if (e.settings.num) {
+    if (e.num) {
       e.indicators[e.settings.slideIndex].classList.add('current-indicator')
     }
     if (e.settings.transition === "pan") {
-      e.slider.children[1].style.transform = 'translateX(-' + e.settings.slideIndex / e.settings.num * 100 + '00%)'
+      e.slider.children[1].style.transform = 'translateX(-' + e.settings.slideIndex / e.num * 100 + '00%)'
     }
 
     e.intervalPrevAnim = setTimeout(function () {
       e.slider.classList.remove('animating')
     }, 1000)
-    if (e.settings.paused === false) {
-      autoSlide(e)
-    }
+    autoSlide(e)
   }
 }
 
@@ -236,17 +239,19 @@ function updateIndex(e, n) {
 }
 
 function autoSlide(e) {
-  e.intervalSlideChange = setTimeout(function () {
-    var num = e.settings.slideIndex + 1
-    updateIndex(e, num)
-    sliderChange(e)
-  }, e.settings.duration)
+  if (e.settings.paused === false) {
+    e.intervalSlideChange = setTimeout(function () {
+      var num = e.settings.slideIndex + 1
+      updateIndex(e, num)
+      sliderChange(e)
+    }, e.settings.duration)
+  }
 }
 
 function clearTimeouts(e) {
   clearTimeout(e.intervalSlideChange)
   clearTimeout(e.intervalPrevAnim)
-  for (var i = 0; i < e.settings.num; i++) {
+  for (var i = 0; i < e.num; i++) {
     e.slider.classList.remove('animating')
   }
 }
