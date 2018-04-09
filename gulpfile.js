20332bbb7bf4259795d61d70ed10a7e391e0d0fa
michael <michael@iseardmedia.com>
2018-04-09 13:37:00 +0200

    reconfigure build process

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,52 @@ var rename = require("gulp-rename")
 // folders
 let folder = {
   src: 'src/',
-  dist: 'dist/'
+  dist: 'dist/',
+  test: 'test/',
+  maps: '../map/'
+  
+}
+
+function imageTask(src, dest, cb) {
+  pump([
+    gulp.src(src),
+    image(),
+    gulp.dest(dest),
+    browserSync.stream()
+  ], cb)
+}
+
+function sassTask(src, dest, cb) {
+  pump([
+    gulp.src(src),
+    sourcemaps.init(),
+    sass(),
+    autoprefixer({
+      browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+    }),
+    cleanCSS({
+      compatibility: 'ie10'
+    }),
+    rename('citrus-slider.min.css'),
+    sourcemaps.write(folder.maps),
+    gulp.dest(dest),
+    browserSync.stream()
+  ],
+  cb
+);
+}
+
+function jsTask(src, dest, cb) {
+  pump([
+    gulp.src(src),
+    sourcemaps.init(),
+    uglify(),
+    rename('citrus-slider.min.js'),
+    sourcemaps.write(folder.maps),
+    gulp.dest(dest)
+  ],
+  cb
+);
 }
 
 gulp.task('default', function () {
@@ -20,60 +65,24 @@ gulp.task('default', function () {
     server: "./"
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
-  gulp.watch(folder.src + 'img/*', gulp.series('image'))
-  gulp.watch('demo/*', gulp.series('image-demo'))
-  gulp.watch(folder.src + 'sass/*', gulp.series('sass'))
-  gulp.watch(folder.src + 'js/*', gulp.series('js'))
+  gulp.watch(folder.src + 'img/*', ['image-app'])
+  gulp.watch('demo/*', ['image-demo'])
+  gulp.watch(folder.src + 'sass/*.scss', ['sass-main'])
+  gulp.watch(folder.src + 'js/*.js', ['js-main'])
 })
 
-gulp.task('image', function (cb) {
-  pump([
-      gulp.src(folder.src + 'img/*'),
-      image(),
-      gulp.dest(folder.dist + 'img/'),
-      browserSync.stream()
-    ],
-    cb
-  );
+gulp.task('image-app', function (cb) {
+  imageTask(folder.src + 'img/*', folder.dist + 'img/', cb)
 })
 
 gulp.task('image-demo', function (cb) {
-  pump([
-      gulp.src('demo/*', {base: './'}),
-      image(),
-      gulp.dest('./'),
-    ],
-    cb
-  );
+  imageTask(folder.test + '/' + folder.src + '/img/*', folder.test + '/' + folder.dist + '/img', cb)
 })
 
-gulp.task('sass', function (cb) {
-  pump([
-      gulp.src(folder.src + 'sass/*.scss'),
-      sass(),
-      autoprefixer({
-        browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
-      }),
-      cleanCSS({
-        compatibility: 'ie8'
-      }),
-      rename('citrus-slider.min.css'),
-      gulp.dest(folder.dist + 'css'),
-      browserSync.stream()
-    ],
-    cb
-  );
+gulp.task('sass-main', function (cb) {
+  sassTask(folder.src + 'sass/*.scss', folder.dist + 'css/', cb)
 })
 
-gulp.task('js', function (cb) {
-  pump([
-      gulp.src(folder.src + 'js/*.js'),
-      // sourcemaps.init(),
-      uglify(),
-      // sourcemaps.write(),
-      rename('citrus-slider.min.js'),
-      gulp.dest(folder.dist + 'js/')
-    ],
-    cb
-  );
+gulp.task('js-main', function (cb) {
+  jsTask(folder.src + 'js/*.js', folder.dist + 'js/', cb)
 })
